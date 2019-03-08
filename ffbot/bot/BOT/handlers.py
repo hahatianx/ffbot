1e0b9fe71310aa31b6ef06a81421ba66c4461ca9
shenshuhan <shenshuhan@github.com>
2019-03-08 17:52:50 +0800

    first func /echo

--- ffbot/bot/BOT/handlers.py
+++ ffbot/bot/BOT/handlers.py
@@ -4,7 +4,7 @@
 def EchoHandler(*kargs):
     ret_msg = ''
     if len(kargs) != 1:
-        ret_msg = '你的指令好像用错了鸭\n正确用法:/echo <string>\n'
+        ret_msg = '你的指令好像用错了鸭\n正确用法:\n/echo <string>'
     else:
         ret_msg = kargs[0]
     return ret_msg
