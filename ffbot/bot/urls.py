5d6800eb5c8ea8638bdd6bf8733ad047d777672c
shenshuhan <shenshuhan@github.com>
2019-03-08 14:03:15 +0800

    fix

--- ffbot/bot/urls.py
+++ ffbot/bot/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
-from .settings import STATIC_ROOT, STATIC_URL
+from bot.settings import STATIC_ROOT, STATIC_URL
 
 
 urlpatterns = [
