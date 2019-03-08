c90a6cf73d2a03344645068d0c390f9735cc9b9d
shenshuhan <shenshuhan@github.com>
2019-03-08 15:55:51 +0800

    test

--- ffbot/bot/consumer.py
+++ ffbot/bot/consumer.py
@@ -40,5 +40,5 @@ class WSConsumer(AsyncWebsocketConsumer):
     # 1) deserialize msg  2) handle msg 3) serialize msg 4) send
     async def receive(self, text_data):
         msg = json.loads(text_data)
-        self.send(msg)
+        print(msg.decode())
 
