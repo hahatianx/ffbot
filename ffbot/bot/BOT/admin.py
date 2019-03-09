a1ab2f223f2ebc0d40575348a28c21341d2ca94a
shenshuhan <shenshuhan@github.com>
2019-03-09 10:41:46 +0800

    added Class Model

--- ffbot/bot/BOT/admin.py
+++ ffbot/bot/BOT/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from bot.BOT import models
 
 # Register your models here.
+admin.site.register(models.Class)
+class ClassAdmin(admin.ModelAdmin):
+    def __init__(self):
+        pass
 
--- ffbot/bot/BOT/models.py
+++ ffbot/bot/BOT/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Class(models.Model):
+    name = models.CharField(max_length=15)
+    nick = models.CharField(max_length=100)
+    fflog_id = models.IntegerField()
