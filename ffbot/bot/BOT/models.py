4c8e938b3f8c8d387d1354ccebe06609232edea6
shenshuhan <shenshuhan@github.com>
2019-03-12 21:11:33 +0800

    fix unicode

--- ffbot/bot/BOT/models.py
+++ ffbot/bot/BOT/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Class(models.Model):
     name = models.CharField(max_length=15)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 
@@ -21,7 +21,7 @@ class Boss(models.Model):
     boss_id = models.IntegerField()
     add_time = models.BigIntegerField(default=0)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 
