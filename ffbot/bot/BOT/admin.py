526bef8d3522e953fe769e85eaad75d692018830
shenshuhan <shenshuhan@github.com>
2019-03-12 21:01:55 +0800

    fix bugs on admin.py

--- ffbot/bot/BOT/admin.py
+++ ffbot/bot/BOT/admin.py
@@ -2,21 +2,21 @@ from django.contrib import admin
 from bot.BOT import models
 
 # Register your models here.
-admin.site.register(models.Class)
+@admin.register(models.Class)
 class ClassAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
 
 
-admin.site.register(models.NickClass)
+@admin.register(models.NickClass)
 class NickClassAdmin(admin.ModelAdmin):
     list_display = ('id', 'nick_name', 'class_id')
 
 
-admin.site.register(models.Boss)
+@admin.register(models.Boss)
 class BossAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'quest_id', 'boss_id', 'add_time')
 
 
-admin.site.register(models.NickBoss)
+@admin.register(models.NickBoss)
 class NickBossAdmin(admin.ModelAdmin):
     list_display = ('id', 'nick_name', 'boss_id')
