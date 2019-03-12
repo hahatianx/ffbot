68ed9e3d9e818293e72c4f542e12c1899bd83249
shenshuhan <shenshuhan@github.com>
2019-03-12 22:23:57 +0800

    fix

--- ffbot/bot/BOT/bothandlers.py
+++ ffbot/bot/BOT/bothandlers.py
@@ -162,12 +162,14 @@ def DpsHandler(*kargs):
             r_class = class_obj[0].class_id
         if not fail:
             day_index = (int(time.time()) - r_boss.add_time) // (24 * 3600)
-            print(day_index)
-            # msg_dict = get_dps_list(r_boss.quest_id, r_boss.boss_id, r_class.name, day_index)
-            msg_dict = {}
-            ret_msg = '以下是国际服FFLOGS {} 在 {} 中的dps表现:\n'.format(r_class.name, r_boss.name)
-            for k, v in msg_dict.values():
-                ret_msg += '{}%%: {}\n'.format(k, v)
+            #print(day_index)
+            try:
+                msg_dict = get_dps_list(r_boss.quest_id, r_boss.boss_id, r_class.name, day_index)
+                ret_msg = '以下是国际服FFLOGS {} 在 {} 中的dps表现:\n'.format(r_class.name, r_boss.name)
+                for k, v in msg_dict.values():
+                    ret_msg += '{}%%: {}\n'.format(k, v)
+            except:
+                ret_msg = '抓取出现bug'
     return ret_msg[:-1]
 
 
