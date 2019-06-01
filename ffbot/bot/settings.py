a3c45920538342f1e091ac946f6354edbf28f594
shuhan <shuhan.x.shen@gmail.com>
2019-06-01 13:44:14 +0800

    I cannot find my bug, so sad

--- ffbot/bot/settings.py
+++ ffbot/bot/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django_db_reconnect',
+    'django_db_reconnect',
     'channels',
     'bot.BOT',
 ]
